f3211538aae9067c4784fe72a8a941ab221bb2af
Jackie Chan <jackiec1998@gmail.com>
2021-01-14 23:39:51 -0600

    Added some documentation and additional functionality with optinal parameters.

--- api_helpers.py
+++ api_helpers.py
@@ -7,9 +7,14 @@ import time
 import datetime as dt
 import pandas as pd
 import os.path as path
+import time
 
 PUSHSHIFT_REDDIT_URL = "http://api.pushshift.io/reddit"
 
+'''
+    Optional Parameters:
+        All (string): All the arguments given will be appended onto the request.
+'''
 def send_request(**kwargs):
 
     # Default parameters for Pushshift API query.
@@ -43,19 +48,50 @@ def send_request(**kwargs):
         print(request.status_code)
         return None
 
+'''
+    Necessary Parameters: 
+        'start_utc' (int): Start of the study period in UTC. Although it will
+            pick up where it left off if file already exists from most recent
+            UTC timestamp.
+        'end_utc' (int): End of the study period in UTC.
+
+    Optional Parameters:
+        'file_name' (string): Name of the CSV file (don't include .csv).
+        'max_retries' (int): The maximum number of retries on a failed request
+            (default is 5).
+        Other (string): Other parameters, like 'subreddit' will be added into
+            the request.
+'''
 def extract_comments(**kwargs):
 
+    # Get the wanted data points from desired_columns.txt file.
     with open("desired_columns.txt") as file:
         desired_columns = file.read().splitlines()
 
-    CSV_file_name = "conservative_comments.csv"
+    # Define the file name.
+    if "file_name" in kwargs.keys():
+        CSV_file_name = kwargs['file_name'] + ".csv"
+        del kwargs['file_name']
+    
+    else:
+        CSV_file_name = "default.csv"
 
-    # Study period.
-    start_utc = 1604361600 # Nov. 3rd
-    end_utc = 1604966400 # Nov. 10th
+    # Define study period.
+    if "start_utc" in kwargs.keys() and "end_utc" in kwargs.keys():
+
+        start_utc = kwargs['start_utc']
+        end_utc = kwargs['end_utc']
+
+        # start_utc = 1604361600 # Nov. 3rd
+        # end_utc = 1604966400 # Nov. 10th
 
     # Maximum retry attempts.
-    max_retries = 5
+    if "max_retries" in kwargs.keys():
+        max_retries = kwargs['max_retries']
+        del kwargs['max_retries']
+
+    else:
+        max_retries = 5
 
     while True:
 
@@ -68,33 +104,46 @@ def extract_comments(**kwargs):
         objects = None
         retries = 0
 
+        # Used to calculate the change in size with the new request.
         num_rows = df.shape[0]
 
         # Start off where you left off on the CSV.
-        start_utc = df['created_utc'].max()
+        try:
+            start_utc = df['created_utc'].max()
+        except:
+            print("Couldn't find a midway point to start from.")
+            pass
 
+        # Send request time.
+        time_sent_request = time.time()
+
+        # Make a request until successful or reaches max retries.
         while objects is None and retries < max_retries:
-            
-            # Make a request.
             objects = send_request(**kwargs, after=start_utc)
+            retries += 1
 
         if objects is None:
-            print("The request returned nothing, stopping extraction. Check request error.")
+            print("The request returned nothing, stopping extraction. " \
+                + "Check request error. Writing to CSV and exiting.")
 
-        print("Request returned {0} objects".format(len(objects)))
+            df = df.drop_duplicates()
+            df.to_csv(CSV_file_name)
             
-        # Loop the return data, ordered by date.
+            return
+
+        # How long it took to get the request.
+        request_duration = time.time() - time_sent_request
+
+        print("Request returned {0} objects. It took {1} seconds.".format(len(objects), request_duration))
+            
+        # Timing formating of objects.
+        time_formatting = time.time()
+
+        # Loop through the returned data and append to dataframe.
         for object in objects:
 
-            # Grab the creation date.
-            created_utc = object['created_utc']
-
-            # Move forward the creation date based on the most recent object.
-            if created_utc > start_utc:
-                start_utc = created_utc
-            
             # The object falls out of study period.
-            if created_utc > end_utc:
+            if object['created_utc'] > end_utc:
                 continue
 
             # Only keep the columns you want.
@@ -109,9 +158,9 @@ def extract_comments(**kwargs):
 
         df.to_csv(CSV_file_name)
 
-        # Display the number of new items.
+        formatting_duration = time.time() - time_formatting
+
+        # Display the number of new items, time formating, and new line.
         print("{0} - {1} = {2} new objects have been added to the CSV.".format(df.shape[0], num_rows, df.shape[0] - num_rows))
-
+        print("Formating the data took {0} seconds.".format(formatting_duration))
         print("\n")
-
-        # time.sleep(0.5)
--- extract_comments.py
+++ extract_comments.py
@@ -1,3 +1,5 @@
 from api_helpers import *
 
-extract_comments(subreddit="conservative")
+# extract_comments(subreddit="conservative")
+extract_comments(file_name="carleton_comments", subreddit="carletoncollege",
+    start_utc=1606780800, end_utc=1609459200)
